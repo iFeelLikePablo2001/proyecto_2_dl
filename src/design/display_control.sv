957e6876d48b9d4fbf72cc400fc75823c884ae41
iFeelLikePablo2001 <pablocm1805@gmail.com>
2026-05-11 18:29:31 -0600

    integracion con top

--- src/design/display_control.sv
+++ src/design/display_control.sv
@@ -1,11 +1,9 @@
 module display_control (
 
-    input logic [1:0] active_display,
+    input logic active_display,
 
     input logic [3:0] units,
     input logic [3:0] tens,
-    input logic [3:0] hundreds,
-    input logic [3:0] thousands,
 
     output logic [3:0] digit_out
 );
@@ -16,8 +14,6 @@ always_comb begin
 
         2'd0: digit_out = units;
         2'd1: digit_out = tens;
-        2'd2: digit_out = hundreds;
-        2'd3: digit_out = thousands;
 
     endcase
 
